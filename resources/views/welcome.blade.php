5a5616dc6bc038d444eb6b945a9cf184042881b7
franciscoLagos18615 <francisco.lagos.s@usach.cl>
2017-05-19 12:47:15 -0400

    cambie el titulo para probar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                ONEMI DBD
+                ONEMI DBD, algo para probar git
                 </div>
 
                 <div class="links">
